542a8bd00e8e7b8eae4fa2a0e1d63ee4ddc0913c
bravoalpha79 <igor.basuga@gmail.com>
2022-09-10 19:45:55 +0200

    Link Summernote to PostAdmin (content field)

--- educational_pages/admin.py
+++ educational_pages/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
-class PostAdmin(admin.ModelAdmin):
+# Apply summernote to all TextField in model.
+class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = "content"
+
     list_display = (
         "title",
         "slug",
