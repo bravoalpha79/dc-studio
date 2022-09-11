5499b3e5b6df4f7cd624383224336c3604007595
bravoalpha79 <igor.basuga@gmail.com>
2022-09-11 21:29:06 +0200

    Add Heroku host to ALLOWED_HOSTS

--- dc_studio/settings.py
+++ dc_studio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", os.environ.get("HEROKU_HOST")]
 
 
 # Application definition
