50f7836dbcadd3c4acfad9c343f670e20c6aaf12
Igor Basuga <igor.basuga@gmail.com>
2022-10-31 22:49:58 +0100

    Add pagination to search results

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
+from itertools import chain
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.contrib.postgres.search import SearchVector
 
 from educational_pages.models import Post
@@ -12,17 +14,22 @@ def index(request):
 
 def search(request):
     q = request.GET["query"] if "query" in request.GET else None
-    posts = None
-    links = None
+    results = None
 
     if q:
         posts = Post.objects.annotate(search=SearchVector("title", "content")).filter(search=q)
         links = MediaLink.objects.annotate(search=SearchVector("title", "summary")).filter(search=q)
+        results = list(chain(posts, links))
+
+        paginator = Paginator(results, 5)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
     
     context = {
         "query": q,
-        "posts": posts,
-        "links": links
+        "results": results,
+        "page_obj": page_obj 
     }
 
     return render(request, "home/search.html", context)
