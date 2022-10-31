6991ad3e674b304fccd01189989a0a5cf1567115
Igor Basuga <igor.basuga@gmail.com>
2022-10-31 22:25:37 +0100

    Add pagination to educational_pages

--- educational_pages/views.py
+++ educational_pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from .models import Post
 
 def all_posts(request):
     posts = Post.objects.all().order_by("position")
-    return render(request, "educational_pages/all_posts.html", {"posts": posts})
+    
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
+    return render(request, "educational_pages/all_posts.html", {"page_obj": page_obj})
 
 def view_post(request, post_slug):
     post = Post.objects.get(slug=post_slug)
